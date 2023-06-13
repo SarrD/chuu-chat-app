cd94d3efff88d7f19054f17d75e5fc677a0bf799
SarrD <leroidave@gmail.com>
2023-06-13 17:55:56 +0200

    fix : la verification admin se base sur le role contenu dans le token a la place d'un cham id_role_admin dans le body

--- back/src/middlewares/isAdmin.js
+++ back/src/middlewares/isAdmin.js
@@ -2,12 +2,10 @@ var express = require('express');
 
 
 exports.isAdmin = (req, res, next) => {
-	console.log(req.body)
-	console.log('req id role admin', req.body.id_role_admin)
 
-    if (parseInt(req.body.id_role_admin) === 2) {
+    if (parseInt(req.user.id_role) === 2) {
     	return next();
 	}else {
-		return res.status(400).json({ message: "You are not an administrator" });
+		return res.status(401).json({ message: "You are not an administrator" });
 	}
 }
